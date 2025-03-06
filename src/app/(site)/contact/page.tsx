b820137d1d53bad685d22175aa7cea5b6062b154
Bleriot Noguia <nstevebleriot@yahoo.fr>
2025-03-06 18:37:34 +0100

    Update Contact page with French localization and refined metadata

--- src/app/(site)/contact/page.tsx
+++ src/app/(site)/contact/page.tsx
@@ -3,15 +3,14 @@ import Contact from "@/components/Contact";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title:
-    "Contact Page | Play SaaS Starter Kit and Boilerplate for Next.js",
-  description: "This is contact page description",
+  title: "Contactez-nous | Go to digital, votre partenaire numérique",
+  description: "Contactez-nous pour plus d'informations",
 };
 
 const ContactPage = () => {
   return (
     <>
-      <Breadcrumb pageName="Contact Page" />
+      <Breadcrumb pageName="Contactez-nous" />
 
       <Contact />
     </>
