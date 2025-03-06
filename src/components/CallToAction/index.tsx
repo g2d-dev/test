a749278c5e37800d3d7d2c79a4e0cb06afb69bf1
Bleriot Noguia <nstevebleriot@yahoo.fr>
2025-03-06 17:41:16 +0100

    Update Call to Action section with French localization and refined messaging

--- src/components/CallToAction/index.tsx
+++ src/components/CallToAction/index.tsx
@@ -9,21 +9,22 @@ const CallToAction = () => {
             <div className="w-full px-4">
               <div className="mx-auto max-w-[570px] text-center">
                 <h2 className="mb-2.5 text-3xl font-bold text-white md:text-[38px] md:leading-[1.44]">
-                  <span>What Are You Looking For?</span>
-                  <span className="text-3xl font-normal md:text-[40px]">
-                    {" "}
-                    Get Started Now{" "}
-                  </span>
+                  <span>Découvrez nos Services Numériques</span>
                 </h2>
-                <p className="mx-auto mb-6 max-w-[515px] text-base leading-[1.5] text-white">
-                  There are many variations of passages of Lorem Ipsum but the
-                  majority have suffered in some form.
+                <p className="mx-auto mb-2.5 max-w-[515px] text-base leading-[1.5] text-white">
+                  Nous offrons des solutions flexibles et sur mesure pour votre
+                  transformation digitale : sites vitrines, e-commerce,
+                  applications ou maintenance.
+                </p>
+
+                <p className="mb-6 text-xl font-normal text-white md:text-2xl">
+                  Choisissez votre formule et démarrez dès aujourd’hui !
                 </p>
                 <Link
-                  href="/"
+                  href="/#pricing"
                   className="inline-block rounded-md border border-transparent bg-secondary px-7 py-3 text-base font-medium text-white transition hover:bg-[#0BB489]"
                 >
-                  Start using Play
+                  En savoir plus
                 </Link>
               </div>
             </div>
